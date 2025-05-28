57f1d8ebad831b47dbfec99159eb861b84a5088f
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2025-05-28 17:53:04 +0900

    あすけんから間食情報を取得する処理を追加

--- src/asken.py
+++ src/asken.py
@@ -1,8 +1,11 @@
+from typing import Optional
+
 from bs4 import BeautifulSoup
 from pydantic import BaseModel
 import requests
 
 from utils import remove_unit
+from const import MEAL_TYPES
 
 
 ASKEN_URL = "https://www.asken.jp"
@@ -11,23 +14,24 @@ ASKEN_URL = "https://www.asken.jp"
 class FoodLog(BaseModel):
     date: str
     meal_type_id: int
-    calories: float  # カロリー(kcal)
-    protein: float  # タンパク質(g)
-    fat: float  # 脂質(g)
-    carbs: float  # 炭水化物(g)
-    calcium: float  # カルシウム(mg)
-    magnesium: float  # マグネシウム(mg)
-    iron: float  # 鉄分(mg)
-    zinc: float  # 亜鉛(mg)
-    vitamin_a: float  # ビタミンA(μg)
-    vitamin_d: float  # ビタミンD(μg)
-    vitamin_b1: float  # ビタミンB1(mg)
-    vitamin_b2: float  # ビタミンB2(mg)
-    vitamin_b6: float  # ビタミンB6(mg)
-    vitamin_c: float  # ビタミンC(mg)
-    fiber: float  # 食物繊維(g)
-    saturatedFat: float  # 飽和脂肪酸(g)
-    solt: float  # 食塩相当量(g)
+    calories: float = 0.0  # カロリー(kcal)
+    protein: float = 0.0  # タンパク質(g)
+    fat: float = 0.0  # 脂質(g)
+    carbs: float = 0.0  # 炭水化物(g)
+    calcium: float = 0.0  # カルシウム(mg)
+    magnesium: float = 0.0  # マグネシウム(mg)
+    iron: float = 0.0  # 鉄分(mg)
+    zinc: float = 0.0  # 亜鉛(mg)
+    vitamin_a: float = 0.0  # ビタミンA(μg)
+    vitamin_d: float = 0.0  # ビタミンD(μg)
+    vitamin_b1: float = 0.0  # ビタミンB1(mg)
+    vitamin_b2: float = 0.0  # ビタミンB2(mg)
+    vitamin_b6: float = 0.0  # ビタミンB6(mg)
+    vitamin_c: float = 0.0  # ビタミンC(mg)
+    fiber: float = 0.0  # 食物繊維(g)
+    saturatedFat: float = 0.0  # 飽和脂肪酸(g)
+    solt: float = 0.0  # 食塩相当量(g)
+    logged: bool = False  # 食事記録が登録済かどうか
 
 
 NUTRITIONS = {
@@ -55,6 +59,14 @@ class Asken:
     def __init__(self, email: str, password: str):
         self.session = self.login(email, password)
 
+    @staticmethod
+    def _headers() -> dict:
+        """Return headers for requests."""
+        return {
+            # User-Agentでアクセス制限をしているようで指定しないと403エラーが返ってくる
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        }
+
     def login(self, email: str, password: str) -> requests.Session:
         """Login to Asken and return a session."""
 
@@ -71,35 +83,122 @@ class Asken:
             "data[Submit][submit][x]": 19,
         }
 
-        headers = {
-            # User-Agentでアクセス制限をしているようで指定しないと403エラーが返ってくる
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        }
-
-        response = session.post(login_url, headers=headers, data=payload)
+        response = session.post(login_url, headers=self._headers(), data=payload)
         response.raise_for_status()  # Check if the request was successful
 
         return session
 
-    def fetch_food_log(self, date: str, meal_type_id: int) -> FoodLog:
+    def fetch_food_log(self, date: str, meal_type_id: Optional[int] = None) -> FoodLog:
         """
-        Fetch food log for a specific date and meal type.
+        Fetch food log for a specific date.
+        Args:
+            date (str): Date in the format 'YYYY-MM-DD'.
+            meal_type_id (Optional[int]): Meal type ID (1: 朝食, 2: 昼食, 3: 夕食, 4: 間食, 5: 1日分). If None, fetches all total food log.
+        Returns:
+            FoodLog: Parsed food log data.
+        """
+        if meal_type_id in [1, 2, 3]:
+            return self.fetch_one_meal_log(date, meal_type_id)
+        elif meal_type_id == 4:
+            return self.fetch_snack_log(date)
+        elif meal_type_id == 5:
+            return self.fetch_daily_food_log(date)
+
+    def fetch_one_meal_log(self, date: str, meal_type_id: int) -> FoodLog:
+        """
+        Fetch one meal log for a specific date and meal type.
         Args:
             date (str): Date in the format 'YYYY-MM-DD'.
             meal_type_id (int): Meal type ID (1: 朝食, 2: 昼食, 3: 夕食).
         Returns:
             FoodLog: Parsed food log data.
         """
+        advice_url = (
+            f"{ASKEN_URL}/wsp/advice/{date}/{MEAL_TYPES[meal_type_id]['asken_id']}"
+        )
+        response = self.session.get(url=advice_url, headers=self._headers())
+        response.raise_for_status()
 
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        }
-        advice_url = f"{ASKEN_URL}/wsp/advice/{date}/{meal_type_id + 2}"
-        response = self.session.get(url=advice_url, headers=headers)
-        response.raise_for_status()  # Check if the request was successful
-        soup = BeautifulSoup(response.text, "html.parser")
+        html = response.text
+        if "食事記録が無いためアドバイスが計算できません" in html:
+            return FoodLog(**{"date": date, "meal_type_id": meal_type_id})
 
-        nutritions = {"date": date, "meal_type_id": meal_type_id}
+        nutritions = self._scrape_food_log(html)
+        nutritions["meal_type_id"] = meal_type_id
+        nutritions["date"] = date
+
+        food_log = FoodLog(**nutritions)
+        food_log.logged = True
+
+        return food_log
+
+    def fetch_daily_food_log(self, date: str) -> FoodLog:
+        """
+        Fetch daily food log for a specific date.
+        Args:
+            date (str): Date in the format 'YYYY-MM-DD'.
+        Returns:
+            FoodLog: Parsed food log data.
+        """
+        advice_url = f"{ASKEN_URL}/wsp/advice/{date}"
+        response = self.session.get(url=advice_url, headers=self._headers())
+        response.raise_for_status()
+
+        html = response.text
+        if "食事記録が無いためアドバイスが計算できません" in html:
+            return FoodLog()
+
+        nutritions = self._scrape_food_log(html)
+        nutritions["meal_type_id"] = 5  # Set meal type ID for daily log
+        nutritions["date"] = date
+
+        food_log = FoodLog(**nutritions)
+        food_log.logged = True
+
+        return food_log
+
+    def fetch_snack_log(self, date: str) -> FoodLog:
+        """
+        Fetch snack log for a specific date.
+        Args:
+            date (str): Date in the format 'YYYY-MM-DD'.
+        Returns:
+            FoodLog: Parsed snack log data.
+        """
+        nutritions = self.fetch_daily_food_log(date).model_dump()
+        nutritions["meal_type_id"] = 4  # Set meal type ID for snack log
+        for meal_type_id in [1, 2, 3]:
+            one_meal_log = self.fetch_one_meal_log(date, meal_type_id).model_dump()
+            nutritions["calories"] -= one_meal_log["calories"]
+            nutritions["protein"] -= one_meal_log["protein"]
+            nutritions["fat"] -= one_meal_log["fat"]
+            nutritions["carbs"] -= one_meal_log["carbs"]
+            nutritions["calcium"] -= one_meal_log["calcium"]
+            nutritions["magnesium"] -= one_meal_log["magnesium"]
+            nutritions["iron"] -= one_meal_log["iron"]
+            nutritions["zinc"] -= one_meal_log["zinc"]
+            nutritions["vitamin_a"] -= one_meal_log["vitamin_a"]
+            nutritions["vitamin_d"] -= one_meal_log["vitamin_d"]
+            nutritions["vitamin_b1"] -= one_meal_log["vitamin_b1"]
+            nutritions["vitamin_b2"] -= one_meal_log["vitamin_b2"]
+            nutritions["vitamin_b6"] -= one_meal_log["vitamin_b6"]
+            nutritions["vitamin_c"] -= one_meal_log["vitamin_c"]
+            nutritions["fiber"] -= one_meal_log["fiber"]
+            nutritions["saturatedFat"] -= one_meal_log["saturatedFat"]
+            nutritions["solt"] -= one_meal_log["solt"]
+
+        return FoodLog(**nutritions)
+
+    def _scrape_food_log(self, html: str) -> dict:
+        """
+        Scrape food log data from HTML content.
+        Args:
+            html (str): HTML content of the food log page.
+        Returns:
+            FoodLog: Parsed food log data.
+        """
+        soup = BeautifulSoup(html, "html.parser")
+        nutritions = {"date": ""}
         nutritions_ele = soup.find_all("li", class_="line_left")
         for nutrition_ele in nutritions_ele:
             nutrition_name = nutrition_ele.find("li", class_="title").text.strip()
@@ -108,6 +207,4 @@ class Asken:
             if nutrition_name in NUTRITIONS:
                 nutritions[NUTRITIONS[nutrition_name]] = remove_unit(nutrition_value)
 
-        food_log = FoodLog(**nutritions)
-
-        return food_log
+        return nutritions
--- src/const.py
+++ src/const.py
@@ -1,4 +1,5 @@
 FITBIT_API_URL = "https://api.fitbit.com"
+
 UNITS = {
     "mg": {"word_cnt": 2},
     "μg": {"word_cnt": 2},
@@ -6,4 +7,27 @@ UNITS = {
     "g": {"word_cnt": 1},
     "kcal": {"word_cnt": 4},
     "cal": {"word_cnt": 3},
+}
+
+MEAL_TYPES = {
+    1: {
+        "name": "朝食（あすけん）",
+        "asken_id": 3,
+        "fitbit_id": 1,
+    },
+    2: {
+        "name": "昼食（あすけん）",
+        "asken_id": 4,
+        "fitbit_id": 3,
+    },
+    3: {
+        "name": "夕食（あすけん）",
+        "asken_id": 5,
+        "fitbit_id": 5,
+    },
+    4: {
+        "name": "間食（あすけん）",
+        "asken_id": 6,
+        "fitbit_id": 7,
+    },
 }
